ad019cf6bc45fa2a1dc277f368854dce7c1135e6
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-29 16:14:40 -0300

    logout redirect

--- src/views/chatroom-view.vue
+++ src/views/chatroom-view.vue
@@ -2,4 +2,17 @@
   <h1>Chatroom</h1>
 </template>
 
-<script setup></script>
+<script setup>
+import getUser from '../composables/getUser'
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const { user } = getUser()
+
+watch(user, () => {
+  if (!user.value) {
+    router.push({ name: 'Welcome' })
+  }
+})
+</script>
