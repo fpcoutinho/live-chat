d3c5ab524f00c64b0c95f26c398c3601da280c94
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-29 16:46:37 -0300

    chatroom mock

--- src/views/chatroom-view.vue
+++ src/views/chatroom-view.vue
@@ -1,5 +1,41 @@
 <template>
   <h1>Chatroom</h1>
+  <div class="chat-window mt-4">
+    <div class="chat chat-start">
+      <div class="chat-header">
+        Obi-Wan Kenobi
+        <time class="text-xs opacity-50">2 hours ago</time>
+      </div>
+      <div class="chat-bubble chat-bubble-primary">You were the Chosen One!</div>
+      <div class="chat-footer opacity-50">
+        Seen
+      </div>
+    </div>
+    <div class="chat chat-start">
+      <div class="chat-header">
+        Obi-Wan Kenobi
+        <time class="text-xs opacity-50">2 hour ago</time>
+      </div>
+      <div class="chat-bubble chat-bubble-primary">I loved you.</div>
+      <div class="chat-footer opacity-50">
+        Delivered
+      </div>
+    </div>
+    <div class="chat chat-end">
+      <div class="chat-header">
+        Anakin
+        <time class="text-xs opacity-50">12:46</time>
+      </div>
+      <div class="chat-bubble chat-bubble-neutral">I hate you!</div>
+      <div class="chat-footer opacity-50">
+        Seen at 12:46
+      </div>
+    </div>
+    <div class="chat-input w-full join">
+      <textarea class="textarea textarea-bordered join-item w-full" rows="1" placeholder="Type here"></textarea>
+      <button class="btn join-item rounded-r-full">Send</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -16,3 +52,24 @@ watch(user, () => {
   }
 })
 </script>
+
+<style>
+.chat-window {
+  width: 42rem;
+}
+
+.chat-input {
+  .textarea {
+    resize: none;
+  }
+
+  .textarea::-webkit-scrollbar {
+    display: none;
+  }
+
+  .textarea {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+}
+</style>
